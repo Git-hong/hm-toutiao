3cc0bc13da5fff987545a5a8e8c092cc3b03bfaa
Git-hong <2935079864@qq.com>
2019-08-01 21:36:37 +0800

    登录模块和组件

--- src/main.js
+++ src/main.js
@@ -5,10 +5,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 引入Element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// @是别名
+import router from '@/router'
+
 // tip 提示信息  production 生成环境
 Vue.config.productionTip = false // 按开发(详细一些)环境来提示
+Vue.use(ElementUI)
 
 // 根实例
 new Vue({
+  // 路由挂载
+  router,
   render: h => h(App)
 }).$mount('#app') // mount挂载
