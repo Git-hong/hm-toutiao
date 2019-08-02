bc5e877cadf846cd6c27b69d05785a632a90b65c
Git-hong <2935079864@qq.com>
2019-08-02 22:52:49 +0800

    初始化欢迎界面

--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,15 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 import Login from '@/views/login'
+import Home from '@/views/home'
 
 Vue.use(VueRouter)
 const router = new VueRouter({
   // 路由规则
   //   name给当前路由规则取别名  $router.push('/login')或者$router.push({name:'login'})
   routes: [
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
-    }
+    { path: '/login', name: 'login', component: Login },
+    { path: '/', name: 'home', component: Home }
   ]
 })
 
---
+++ src/views/home/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <el-container class="wrapper">
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <el-header>
+        <span class="el-icon-s-fold"></span>
+        <span class="text">江苏传智播客科技教育有限公司</span>
+      </el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="less">
+//标签的名称其实就是解析后标签的类名
+.wrapper {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  .el-aside {
+    background: #002033;
+  }
+  .el-header {
+    border-bottom: 1px solid #ddd;
+    line-height: 60px;
+    .el-icon-s-fold {
+      font-size: 30px;
+      vertical-align:middle;
+      .text {
+          vertical-align:middle;
+      }
+    }
+  }
+}
+</style>
