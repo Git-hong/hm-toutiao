1ab0f351e6fa3edcfae7cf935afd58f2d38b8cd2
Git-hong <2935079864@qq.com>
2019-08-02 21:00:33 +0800

    登录页面完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,22 +1,27 @@
 <template>
   <div class="container">
     <el-card class="my-card">
-      <img src="../../assets/images/logo_index.png" alt="">
+      <img src="../../assets/images/logo_index.png" alt />
       <!-- 表单 -->
-      <el-form :model="loginForm">
-        <el-form-item>
-          <el-input></el-input>
+      <el-form ref="loginForm" :model="loginForm" :rules="loginRules" status-icon>
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input
+            v-model="loginForm.code"
+            style="width:236px;margin-right:10px"
+            placeholder="请输入验证码"
+          ></el-input>
+          <el-button>发送验证码</el-button>
         </el-form-item>
         <el-form-item>
-          <el-input></el-input>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-input></el-input>
+          <el-button @click="login" style="width:100%" type="primary">登 录</el-button>
         </el-form-item>
-        <el-form-item>
-          <el-input></el-input>
-        </el-form-item>
-      <el-form>
+      </el-form>
     </el-card>
   </div>
 </template>
@@ -24,8 +29,48 @@
 <script>
 export default {
   data () {
+    // 申明校验函数
+    const checkMobile = (rule, value, callback) => {
+      // 校验手机号 1开头 第二位 3-9  9位数字
+      if (!/^1[3-9]\d{9}$/.test(value)) {
+        return callback(new Error('手机格式不对'))
+      }
+      callback()
+    }
     return {
-      loginForm: {}
+      // 表单对象数据
+      loginForm: {
+        mobile: '13911111111',
+        code: '246810'
+      },
+      // 表单验证规则数据
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '是6位', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      // 对整个表单进行校验
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+          // 请求登录接口
+          this.$http.post('http://ttapi.research.itcast.cn/mp/v1_0/articles', this.loginForm).then(res => {
+            // 响应对象（包含响应主体）
+            console.log(res.data)
+          }).catch(() => {
+            // 错误提示
+            this.$message.error('手机号或验证码错误')
+          })
+        }
+      })
     }
   }
 }
@@ -49,7 +94,7 @@ export default {
   img {
     display: block;
     width: 200px;
-    margin: 0 auto 30px;
+    margin: 0 auto 20px;
   }
 }
 </style>
